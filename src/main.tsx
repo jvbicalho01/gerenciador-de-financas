08a3588a63eb8e5e246e49aef936dc619687e911
João Vitor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2024-01-02 18:57:37 -0300

    configurando modal de nova transação

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createServer } from "miragejs"
+import { Model, createServer } from "miragejs"
 import App from './App.tsx'
 
 createServer({
+
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Desenvolvimento",
+          amount: 6000,
+          createdAt: new Date("2024-01-02 18:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1600,
+          createdAt: new Date("2024-01-10 10:00:00"),
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return {
-        id: 1,
-        title: "Transaction 1",
-        amount: 400,
-        type: "deposit",
-        category: "Food",
-        createdAt: new Date(),
-      }
+      return this.schema.all("transaction")
+    })
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
     })
   }
 })
